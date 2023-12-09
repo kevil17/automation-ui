e4023654a41a22cb93827acce074c573554e8eca
Kevin Marcelo Lopez Tordoya <kevin.inck@gmail.com>
2023-12-08 21:21:21 -0400

    change user

--- src/test/java/base/TodoTestNewUser.java
+++ src/test/java/base/TodoTestNewUser.java
@@ -8,7 +8,7 @@ import task.validations.IsLogOut;
 public class TodoTestNewUser extends BaseTest{
     @Test
     public void checkSuccessRegister(){
-        TodoLyRegister.as(webDriver,"Kevin Lopez","kevin.inck@gmail.com","kevin123456");
+        TodoLyRegister.as(webDriver,"Kevin Lopez","kevinick.inck@gmail.com","kevin123456");
         Assert.assertTrue(IsLogOut.visible(webDriver));
     }
 }
